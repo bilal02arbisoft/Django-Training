aeaebe6a8be96c8a1ca170d723f2e6c3f1154d37
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-18 16:48:07 +0500

    Created django form named contactus thorugh Forms class and created a function view to render it

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.db.models import F
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from .models import Question, Choice
 from django.urls import reverse
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 
 class IndexView(generic.ListView):
@@ -17,6 +19,7 @@ class IndexView(generic.ListView):
 
 
 class DetailView(generic.DetailView):
+
     model = Question
     template_name = 'polls/detail.html'
 
@@ -26,11 +29,44 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
+def contact_view(request):
+
+    if request.method == "POST":
+
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["message"]
+            sender = form.cleaned_data["sender"]
+            cc_myself = form.cleaned_data["cc_myself"]
+
+            recipients = []
+            if cc_myself:
+
+                recipients.append(sender)
+
+            # send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect(reverse('polls:thanks'))
+    else:
+
+        form = ContactForm()
+
+    return render(request, 'polls/contact.html', {'form': form})
+
+
+def thanks(request):
+
+    return HttpResponse('Your Contact Form Submitted Successfully')
+
+
 def vote(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     try:
+
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
     except (KeyError, Choice.DoesNotExist):
 
         return render(
@@ -41,7 +77,9 @@ def vote(request, question_id):
                 'error_message': "You didn't select a choice.",
             },
         )
+
     else:
+
         selected_choice.votes = F('votes') + 1
         selected_choice.save()
 
