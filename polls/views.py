0a9f6e2434e28fe558fbba79b715987962317f48
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-18 13:43:42 +0500

    urls file modified according to changing of views

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpRequest, HttpResponse
+from django.db.models import F
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
-from .models import Question
+from .models import Question, Choice
+from django.urls import reverse
 
 
 class IndexView(generic.ListView):
@@ -23,6 +26,23 @@ class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
 
 
-def vote(request: HttpRequest, question_id):
+def vote(request, question_id):
 
-    return HttpResponse(f'You are voting for question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
