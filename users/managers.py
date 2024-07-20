d571bcbdc9cb4ebc1cf5cd8d1e31b8b476994d61
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-07-20 12:39:49 +0500

    Created CustomUser model and CustomUserManager to create normal and superusers by inheriting from BaseUserMManager and AbstractBaseUser

---
+++ users/managers.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, first_name=None, last_name=None, **extra_fields):
+
+        if not email:
+
+            raise ValueError('The Email field must be set')
+
+        if not first_name:
+
+            raise ValueError('The First Name field must be set')
+
+        if not last_name:
+
+            raise ValueError('The Last Name field must be set')
+
+        if not password:
+
+            raise ValueError('The Password field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, first_name=None, last_name=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not email:
+
+            raise ValueError('The Email field must be set')
+
+        if not first_name:
+
+            raise ValueError('The First Name field must be set')
+
+        if not last_name:
+
+            raise ValueError('The Last Name field must be set')
+
+        if not password:
+
+            raise ValueError('The Password field must be set')
+
+        if extra_fields.get('is_staff') is not True:
+
+            raise ValueError('Superuser must have is_staff=True')
+
+        if extra_fields.get('is_superuser') is not True:
+
+            raise ValueError('Superuser must have is_superuser=True')
+
+        return self.create_user(email, password, first_name, last_name, **extra_fields)
+
+
+
